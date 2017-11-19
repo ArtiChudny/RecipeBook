5f01adf8f16526f0ba51f33b0ce04d78af406803
Artsiom <chudnyi3@mail.ru>
2017-11-20 02:48:19 +0300

    [RB_0.2] Adding IoC container StructureMap.MVC5  to display message.

---
+++ RecipeBookMVC/RecipeBook.Business/MsgProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.Business
+{
+    public class MsgProvider:IProvider
+    {
+        public string GetMessage()
+        {
+            return  "Welcome to this website";
+        }
+    }
+}
--- RecipeBookMVC/RecipeBookMVC/Controllers/HomeController.cs
+++ RecipeBookMVC/RecipeBookMVC/Controllers/HomeController.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Web.Mvc;
 using log4net;
+using RecipeBook.Business;
+using RecipeBookMVC.Models;
+using System.Net;
 
 namespace RecipeBookMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILog log = LogManager.GetLogger("Logger");
+        private readonly IProvider provider;
+
+        public HomeController(IProvider _provider)
+        {
+            provider = _provider;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
-            log.Error("Testing Error");
-            return View();
+           
+            try
+            {
+                HomeViewModel model= new HomeViewModel();
+                model.Message = provider.GetMessage();
+                return View(model);
+
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                log.Error(ex.Message);
+                return View("Error");
+
+
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return View("Error");
+
+            }
+           
+           
         }
     }
 }
